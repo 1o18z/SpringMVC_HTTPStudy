cedd9e35d01a06bc3c35350457546fcee445a0df
eunji <qkrdmswl1018@naver.com>
2023-04-04 17:32:56 +0900

    HTTP 요청 메시지 - JSON

--- src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -1,2 +1,75 @@
-package hello.springmvc.basic.request;public class RequestBodyJsonController {
+package hello.springmvc.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    private ObjectMapper objectMapper = new ObjectMapper(); // JSON이니까 ObjecyMapper 있어야 함 !
+
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody = {}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v2")
+    public String requestBodyJsonV2(@RequestBody String messageBody) throws IOException {
+
+        log.info("messageBody = {}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class); // HttpMessageConverter가 Json이면 요 코드를 대신 실행해줌
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData helloData) { // JSON이 그대로 helloData에 들어오게 됨
+        // 만약 @RequestBody를 생략하면 @ModelAttribute가 되어버림! -> HTTP 메시지 바디가 아닌 요청 파라미터를 처리하게 됨 => 생략 불가능
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) {
+        HelloData data = httpEntity.getBody();
+
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) { //ResponseBody가 있으면 HttpmessageConverter가 들어올 때도 적용이 되지만 나갈 때도 적용 된다!
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return data;
+    }
 }
+
+
